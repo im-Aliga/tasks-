3c148fa6e8fab44c76378b1945a3670aa9e9c801
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-06-12 17:55:45 +0400

    tasks

---
+++ task.2/task.2/Program.cs
@@ -0,0 +1,32 @@
+﻿while (true)
+{
+
+
+    char[] bigLetter = { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'X', 'C', 'V', 'B', 'N', 'M' };
+    char[] smallLetter = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+    Console.WriteLine("please entertxt");
+    string textTxT = Console.ReadLine();
+
+    string result = Islettercapital(textTxT, bigLetter, smallLetter);
+
+
+    Console.WriteLine(result);
+
+
+}
+
+
+static string Islettercapital(string textTxT, char[] bigLetter, char[] smallLetter)
+{
+    for (int i = 0; i < smallLetter.Length; i++)
+    {
+        textTxT = textTxT.Replace(bigLetter[i], smallLetter[i]);
+
+
+
+    }
+
+    return textTxT;
+
+
+}
---
+++ task.3/task.3/Program.cs
@@ -0,0 +1,29 @@
+﻿while (true)
+{
+    Console.WriteLine("please enter text");
+    string targetName = Console.ReadLine();
+    Console.WriteLine("please enter  char");
+    char targetLetter = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine(Isletterhere(targetName, targetLetter));
+
+}
+
+
+static string Isletterhere(string targetName, char targetLetter)
+{
+    for (int i = 0; i < targetName.Length; i++)
+    {
+        if (targetName[i] == targetLetter)
+        {
+            return "he";
+        }
+
+
+    }
+
+
+    return "yox";
+
+
+}
